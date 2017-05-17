7f289046ad2a6592a2440a63c0222ba93e80c350
simonesawyer <simonesawyer@users.noreply.github.com>
2017-05-17 13:54:54 -0700

    Update test.java

--- src/test.java
+++ src/test.java
@@ -1,9 +1,22 @@
-/**
- * Created by ros_crherzog on 5/8/2017.
- */
-public class test {
-    public static void main(String[] args){
-        System.out.println("Hello world");
-    }
-}
+import javax.swing.*;
+import java.awt.*;
+import java.io.*;
+import java.net.*;
 
+public class Test{
+
+   public static void main(String[]args){
+      try{
+      SocketServer server= new SocketServer();
+      SocketClient client = new SocketClient();
+      }
+      catch(IOexception e){
+         System.out.print(e);
+      }
+      server.run();
+      client.run();
+   }
+
+
+
+}
